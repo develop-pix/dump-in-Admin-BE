af9fe727bdc375ecdaf11adef00390c84eb2eb63
Zamoca <suntail2002@naver.com>
2023-11-02 23:06:48 +0900

    Chore: TypeORM + postgresql 설정
    - TypeORM과 pg를 nest와 연결
    
    Related to #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,9 +13,17 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       validate,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+    }),
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
