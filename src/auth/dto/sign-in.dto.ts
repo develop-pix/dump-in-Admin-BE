fff5b8f63c52f414c3d073eacb193dd3eb52b1f0
Chooooooo <suntail2002@naver.com>
2023-11-11 04:09:59 +0900

    Feat: 인증 관련 request dto 추가
    - 인증 관련 request dto 추가
    
    Related to #5

---
+++ src/auth/dto/sign-in.dto.ts
@@ -0,0 +1,48 @@
+import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../../user/entity/user.entity';
+import {
+  IsNotEmpty,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class SignInDto {
+  @ApiProperty({
+    description: '어드민이 로그인에 사용할 아이디 필드입니다.',
+    required: true,
+    example: 'admin',
+  })
+  @Expose()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(128)
+  username: string;
+
+  @ApiProperty({
+    description: '어드민 로그인에 사용할 password 필드입니다.',
+    required: true,
+    example: 'dump-in123!',
+  })
+  @Expose()
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
+  @MaxLength(30)
+  password: string;
+
+  toEntity(): User {
+    const props = {
+      username: this.username,
+      password: this.password,
+    };
+
+    return User.of(props);
+  }
+}
