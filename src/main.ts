e85e2b13b45b4477c6e1e61bb5a6e5a31e7c4b8f
Chooooooo <suntail2002@naver.com>
2023-11-12 03:11:21 +0900

    Chore: 세션DB 연결에서 메모리 연결로 변경
    - 세션 DB 연결 실패
    
    Related to #5

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import * as session from 'express-session';
 import * as passport from 'passport';
+// import * as connectPgSimple from 'connect-pg-simple';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  // const pgSession = connectPgSimple(session);
   const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
   const validationPipeOptions = {
@@ -30,6 +32,12 @@ async function bootstrap(): Promise<void> {
   };
 
   const sessionOptions = {
+    // store: new pgSession({
+    //   conObject: {
+    //     connectionString: process.env.DATABASE_URL,
+    //   },
+    //   tableName: 'session',
+    // }),
     secret: process.env.SET_COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
@@ -46,18 +54,20 @@ async function bootstrap(): Promise<void> {
     .build();
   const document = SwaggerModule.createDocument(app, config);
 
-  app.use(session(sessionOptions))
-  app.use(passport.initialize())
+  app.use(session(sessionOptions));
+  app.use(passport.initialize());
   app.use(passport.session());
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app
     .useGlobalPipes(new ValidationPipe(validationPipeOptions))
     .useGlobalFilters(new HttpExceptionFilter(new Logger()))
-    .enableCors(corsOptions)
+    .enableCors(corsOptions);
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   SwaggerModule.setup('api-docs', app, document);
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
+  console.log('Session Options:', sessionOptions);
   await app.listen(3000);
 }
 bootstrap();
