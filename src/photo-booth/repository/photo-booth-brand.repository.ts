61829e657c6e9f9df66a59fe699ecd2cbbbd9839
Chooooooo <suntail2002@naver.com>
2023-11-28 18:32:03 +0900

    Feat: photo-booth brand 추가
    - 브랜드 관리 레포지토리 추가
    
    Related to #4

---
+++ src/photo-booth/repository/photo-booth-brand.repository.ts
@@ -0,0 +1,13 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PhotoBoothBrand } from '../entity/photo-booth-brand.entity';
+
+@Injectable()
+export class PhotoBoothRawRepository {
+  constructor(
+    @InjectRepository(PhotoBoothBrand)
+    private readonly photoBoothBrandRepository: Repository<PhotoBoothBrand>,
+  ) { }
+
+}
