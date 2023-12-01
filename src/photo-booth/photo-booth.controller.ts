546286799e36e94bc99296c87556e1e230f87dcd
Chooooooo <suntail2002@naver.com>
2023-12-01 21:23:40 +0900

    Feat: 포토부스 리스트, 디테일 메서드 추가
    - findOpenBoothByQueryParam 메서드로 페이지네이션 및 목록보기 추가
    - findOneOpenBooth 메서드로 ID로 디테일 페이지 보기 추가

--- src/photo-booth/photo-booth.controller.ts
+++ src/photo-booth/photo-booth.controller.ts
@@ -1,6 +1,21 @@
-import { Controller, Get, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  Post,
+  Put,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { PhotoBoothService } from './photo-booth.service';
 import { ApiTags } from '@nestjs/swagger';
+import { GetPhotoBoothListDto } from './dto/get-photo-booth-list.dto';
+import { ResponseEntity } from 'src/common/entity/response.entity';
+import { BoothQueryDto } from './dto/get-photo-booth-query.dto';
+import { Page } from '../common/dto/paginated-res.dto';
+import { GetPhotoBoothDetailDto } from './dto/get-photo-booth-detail.dto';
 
 @ApiTags('포토부스')
 @Controller('photo-booth')
@@ -8,34 +23,55 @@ export class PhotoBoothController {
   constructor(private readonly photoBoothService: PhotoBoothService) { }
 
   @Get()
-  async findAllBooth() {
-  }
-
-  @Get()
-  async findBoothByQuery(@Query() request) {
+  async findOpenBoothByQueryParam(
+    @Query() request: BoothQueryDto,
+  ): Promise<ResponseEntity<Page<GetPhotoBoothListDto>>> {
+    const response = await this.photoBoothService.findOpenBoothByQueryParam(request);
+    return ResponseEntity.OK_WITH<Page<GetPhotoBoothListDto>>(
+      '공개된 포토부스 목록을 반환합니다.',
+      response,
+    );
   }
 
   @Get(':id')
-  async findOneBooth(@Param('id') id: string) {
+  async findOneOpenBooth(@Param('id', ParseUUIDPipe) id: string): Promise<ResponseEntity<GetPhotoBoothDetailDto>> {
+    const response = await this.photoBoothService.findOneOpenBooth(id);
+    return ResponseEntity.OK_WITH<GetPhotoBoothDetailDto>(
+      '공개된 포토부스 목록을 반환합니다.',
+      response,
+    );
   }
 
   @Patch(':id')
-  async updateBooth(@Param('id') id: string) {
-  }
+  async updateOpenBooth() { }
 
   @Delete(':id')
-  async deleteBooth(@Param('id') id: string) {
-  }
+  async deleteOpenBooth() { }
+
+  @Get('raw')
+  async findHiddenBoothByQuery() { }
+
+  @Get('raw/:id')
+  async findOneHiddenBooth() { }
+
+  @Put('raw/:id')
+  async moveHiddenToOpenBooth() { }
+
+  @Patch('raw/:id')
+  async updateHiddenBooth() { }
+
+  @Delete('raw/:id')
+  async deleteHiddenBooth() { }
+
+  @Post('brand')
+  async createBrand() { }
 
   @Get('brand')
-  async findAllBrand() {
-  }
+  async findAllBrand() { }
 
   @Get('brand/:id')
-  async findOneBrand(@Param('id') id: string) {
-  }
+  async findOneBrand() { }
 
   @Patch('brand/:id')
-  async updateBrand(@Param('id') id: string) {
-  }
+  async updateBrand() { }
 }
--- src/photo-booth/photo-booth.service.ts
+++ src/photo-booth/photo-booth.service.ts
@@ -1,69 +1,134 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PhotoBoothRepository } from './repository/photo-booth.repository';
+import { PhotoBoothRawRepository } from './repository/photo-booth-raw-data.repository';
+import { GetPhotoBoothListDto } from './dto/get-photo-booth-list.dto';
+import { BoothQueryDto } from './dto/get-photo-booth-query.dto';
+import { Page } from '../common/dto/paginated-res.dto';
+import { GetPhotoBoothDetailDto } from './dto/get-photo-booth-detail.dto';
 
 @Injectable()
 export class PhotoBoothService {
-  async findAllBooth() {
+  constructor(
+    private readonly photoBoothRepository: PhotoBoothRepository,
+    private readonly photoBoothRawRepository: PhotoBoothRawRepository,
+  ) { }
+
+  async findOpenBoothByQueryParam(
+    request: BoothQueryDto,
+  ): Promise<Page<GetPhotoBoothListDto>> {
     /**
-     * @desc PhotoBoothRaw, PhotoBooth에 있는 모든 데이터 반환
+     * @param request - request query string - 지점명, 지역
+     * @param request - request paginated - 항목수, 페이지
+     * @desc 쿼리 파라미터에 맞는 데이터 반환
      */
-    return `This action returns all photoBooth`;
+
+    const { page, take } = request;
+    const [photoBooths, count] =
+      await this.photoBoothRepository.findBoothByOptionAndCount(request);
+
+    if (photoBooths.length === 0) {
+      throw new NotFoundException('공개된 포토부스 지점을 찾지 못했습니다');
+    }
+
+    const photoBoothResult = photoBooths.map(
+      (photoBooth) => new GetPhotoBoothListDto(photoBooth),
+    );
+
+    return new Page<GetPhotoBoothListDto>(page, take, count, photoBoothResult);
   }
 
-  async findBoothByQuery(request) {
+  async findOneOpenBooth(id: string) {
     /**
-    * @param request - request query string - 포토부스명(Brand), 지역, 업종, 공개여부
-    * @desc 쿼리 파라미터에 맞는 데이터 반환 
-    *       - 공개여부가 false라면 raw 데이터를 반환
-    *       - 공개여부가 true라면 포토부스 데이터를 반환
-    */
-    return `This action returns photoBooth By Query`;
+     * @param id - photobooth의 uuid 값
+     * @desc PhotoBooth에 대한 디테일 데이터 반환
+     */
+    const photoBooth = await this.photoBoothRepository.findOneBoothById(id);
+
+    if (!photoBooth) {
+      throw new NotFoundException(`포토부스 지점을 찾지 못했습니다. ID: ${id}`);
+    }
+
+    return new GetPhotoBoothDetailDto(photoBooth);
   }
 
-  async findOneBooth() {
+  async updateOpenBooth() {
     /**
-   * @desc PhotoBoothRaw, PhotoBooth에 대한 디테일 데이터 반환
-   */
-    return `This action returns a photoBooth`;
+     * @param id - id값
+     * @param request - 수정이 필요한 데이터 일부
+     * @desc 포토부스 지점에 대한 데이터 수정
+     */
   }
 
-  async updateBooth() {
+  async deleteOpenBooth() {
     /**
-    * @param id - id값
-    * @param request - 수정이 필요한 데이터
-    * @desc 포토부스 지점에 대한 데이터 수정
-    */
-    return `This action updates a photoBooth`;
+     * @param id - id값
+     * @desc 해당 id의 포토부스 지점 삭제
+     */
   }
 
-  async deleteBooth() {
+  async findHiddenBoothByQuery() {
     /**
-    * @param id - id값
-    * @desc 해당 id의 포토부스 지점 삭제
-    */
-    return `This action removes a photoBooth`;
+     * @param request - request query string - 포토부스명(Brand), 지역, 업종, 공개여부
+     * @desc 쿼리 파라미터에 맞는 데이터 반환
+     *       - 공개여부가 false라면 raw 데이터를 반환
+     *       - 공개여부가 true라면 포토부스 데이터를 반환
+     */
+  }
+
+  async findOneHiddenBooth() {
+    /**
+     * @param request - request query string - 포토부스명(Brand), 지역, 업종, 공개여부
+     * @desc 쿼리 파라미터에 맞는 데이터 반환
+     *       - 공개여부가 false라면 raw 데이터를 반환
+     *       - 공개여부가 true라면 포토부스 데이터를 반환
+     */
+  }
+
+  async moveHiddenToOpenBooth() {
+    /**
+     * @param request - request query string - 포토부스명(Brand), 지역, 업종, 공개여부
+     * @desc 쿼리 파라미터에 맞는 데이터 반환
+     *       - 공개여부가 false라면 raw 데이터를 반환
+     *       - 공개여부가 true라면 포토부스 데이터를 반환
+     */
+  }
+
+  async updateHiddenBooth() {
+    /**
+     * @param request - request query string - 포토부스명(Brand), 지역, 업종, 공개여부
+     * @desc 쿼리 파라미터에 맞는 데이터 반환
+     *       - 공개여부가 false라면 raw 데이터를 반환
+     *       - 공개여부가 true라면 포토부스 데이터를 반환
+     */
+  }
+
+  async deleteHiddenBooth() {
+    /**
+     * @param request - request query string - 포토부스명(Brand), 지역, 업종, 공개여부
+     * @desc 쿼리 파라미터에 맞는 데이터 반환
+     *       - 공개여부가 false라면 raw 데이터를 반환
+     *       - 공개여부가 true라면 포토부스 데이터를 반환
+     */
   }
 
   async findAllBrand() {
     /**
-    * @desc 포토부스 업체에 대한 정보와 업체가 가지고 있는 지점 정보 반환
-    */
-    return `This action returns all photoBooth`;
+     * @desc 포토부스 업체에 대한 정보와 업체가 가지고 있는 지점 정보 반환
+     */
   }
 
   async findOneBrand() {
     /**
-    * @param id - 포토부스 업체에 대한 id
-    * @desc 포토부스 업체의 이름, 대표사진, 지점 목록, 해시태그
-    */
-    return `This action returns a photoBooth`;
+     * @param id - 포토부스 업체에 대한 id
+     * @desc 포토부스 업체의 이름, 대표사진, 지점 목록, 해시태그
+     */
   }
 
   async updateBrand() {
     /**
-    * @param id - 포토부스 업체에 대한 id
-    * @param request - 포토부스 업체에 대한 수정 데이터
-    * @desc 포토부스 업체의 이름, 대표사진, 지점 목록, 해시태그의 데이터를 수정
-    */
-    return `This action updates a photoBooth`;
+     * @param id - 포토부스 업체에 대한 id
+     * @param request - 포토부스 업체에 대한 수정 데이터
+     * @desc 포토부스 업체의 이름, 대표사진, 지점 목록, 해시태그의 데이터를 수정
+     */
   }
 }
