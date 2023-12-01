b8d150f1cc254cc06c60373436d3d2ac70be3d5b
Chooooooo <suntail2002@naver.com>
2023-12-01 21:19:46 +0900

    Feat: 포토부스 레포지토리에 옵션 추가
    - findBoothManyOptions, findBoothOptionsWhere 추가
    - 페이지네이션을 위한 findBoothByOptionAndCount 메서드 추가
    - findOneBoothById로 FindOne 찾기 추가
    
    Related to #4

--- src/photo-booth/repository/photo-booth-raw-data.repository.ts
+++ src/photo-booth/repository/photo-booth-raw-data.repository.ts
@@ -2,7 +2,6 @@ import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PhotoBoothRawData } from '../entity/raw-data.entity';
-import { PhotoBoothBrand } from '../entity/photo-booth-brand.entity';
 
 @Injectable()
 export class PhotoBoothRawRepository {
@@ -11,26 +10,7 @@ export class PhotoBoothRawRepository {
     private readonly photoBoothRawRepository: Repository<PhotoBoothRawData>,
   ) {}
 
-  async findAll(data: PhotoBoothRawData): Promise<PhotoBoothRawData[]> {
-    return await this.photoBoothRawRepository.find();
-  }
-
-  // async findPhotoBoothRawBy(data: PhotoBoothRawData): Promise<PhotoBoothRawData> {
-  //   return await this.photoBoothRawRepository.findOne();
-  // }
-
-  async updateRawToApp(
-    userId: number,
-    data: PhotoBoothRawData,
-  ): Promise<boolean> {
-    const result = await this.photoBoothRawRepository.update(
-      { photo_booth_raw_data_id: userId },
-      data,
-    );
-    return result.affected > 0;
-  }
-
-  private getFindPhotoBoothOptionsWhere(data: PhotoBoothRawData) {
-    return;
+  async findBoothRawByOptionAndCount(): Promise<[PhotoBoothRawData[], number]> {
+    return await this.photoBoothRawRepository.findAndCount();
   }
 }
--- src/photo-booth/repository/photo-booth.repository.ts
+++ src/photo-booth/repository/photo-booth.repository.ts
@@ -1,16 +1,73 @@
-import { Repository } from 'typeorm';
+import {
+  FindManyOptions,
+  FindOptionsSelect,
+  FindOptionsWhere,
+  Repository,
+} from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PhotoBooth } from '../entity/photo-booth.entity';
+import { BoothQueryDto } from '../dto/get-photo-booth-query.dto';
+import { PaginatedProps } from '../../common/dto/paginated-req.dto';
+
+export interface FindBoothOptionWhere {
+  id?: string;
+  name?: string;
+  location?: string;
+}
 
 @Injectable()
 export class PhotoBoothRepository {
   constructor(
     @InjectRepository(PhotoBooth)
     private readonly photoBoothRepository: Repository<PhotoBooth>,
-  ) {}
+  ) { }
 
-  async isExist(data: PhotoBooth): Promise<boolean> {
-    return await this.photoBoothRepository.exist();
+  async findBoothByOptionAndCount(
+    request: BoothQueryDto,
+  ): Promise<[PhotoBooth[], number]> {
+    const options = this.findBoothManyOptions(
+      request.getPageProps(),
+      PhotoBooth.of(request.getQueryProps()),
+    );
+    return await this.photoBoothRepository.findAndCount(options);
+  }
+
+  async findOneBoothById(id: string): Promise<PhotoBooth | null> {
+    const where = this.findBoothOptionsWhere(PhotoBooth.byId({ id }));
+    return await this.photoBoothRepository.findOneBy(where);
+  }
+
+  private findBoothManyOptions(
+    page: PaginatedProps,
+    booth: PhotoBooth,
+  ): FindManyOptions<PhotoBooth> {
+    const { take, skip } = page;
+    const where = this.findBoothOptionsWhere(booth);
+    const relations = { photo_booth_brand: true };
+    const select: FindOptionsSelect<PhotoBooth> = {
+      id: true,
+      name: true,
+      location: true,
+      latitude: true,
+      longitude: true,
+      operation_time: true,
+      road_address: true,
+      street_address: true,
+      photo_booth_brand: {
+        name: true,
+      },
+    };
+    return { where, relations, take, skip, select };
+  }
+
+  private findBoothOptionsWhere(
+    booth: PhotoBooth,
+  ): FindOptionsWhere<PhotoBooth> {
+    return {
+      id: booth.id,
+      location: booth.location,
+      name: booth.name,
+    };
   }
 }
