b2071cda352f4884182d6a5999814e39c3a7fd7d
Chooooooo <suntail2002@naver.com>
2023-12-03 22:28:23 +0900

    Feat: 포토부스 브랜드 entity 정적 메서드 추가
    - 생성, 조회, 수정, 쿼리에 관한 정적 메서드 추가
    
    Related to #4

--- src/photo-booth/entity/photo-booth-brand.entity.ts
+++ src/photo-booth/entity/photo-booth-brand.entity.ts
@@ -7,6 +7,21 @@ import {
   JoinColumn,
 } from 'typeorm';
 import { PhotoBooth } from './photo-booth.entity';
+import { FindBrandOptionProps } from '../dto/get-photo-booth-query.dto';
+
+export interface BrandCreateProps {
+  name: string;
+  main_thumbnail_image_url: string;
+  is_event: boolean;
+}
+
+export interface BrandUpdateProps {
+  name: string;
+  description: string;
+  photo_booth_url: string;
+  main_thumbnail_image_url: string;
+  is_event: boolean;
+}
 
 @Entity('photo_booth_brand')
 export class PhotoBoothBrand {
@@ -40,6 +55,63 @@ export class PhotoBoothBrand {
       photoBoothHashtag.photo_booth_brand,
   )
   photo_booth_hashtags: PhotoBoothHashtag[];
+
+  static create({
+    name,
+    main_thumbnail_image_url,
+    is_event,
+  }: BrandCreateProps): PhotoBoothBrand {
+    const brand = new PhotoBoothBrand();
+
+    brand.name = name;
+    brand.main_thumbnail_image_url = main_thumbnail_image_url;
+    brand.is_event = is_event;
+
+    return brand;
+  }
+
+  static updateBy({
+    name,
+    description,
+    photo_booth_url,
+    main_thumbnail_image_url,
+    is_event,
+  }: BrandUpdateProps): PhotoBoothBrand {
+    const brand = new PhotoBoothBrand();
+
+    brand.name = name;
+    brand.description = description;
+    brand.photo_booth_url = photo_booth_url;
+    brand.main_thumbnail_image_url = main_thumbnail_image_url;
+    brand.is_event = is_event;
+
+    return brand;
+  }
+
+  static of({ name, is_event }: FindBrandOptionProps): PhotoBoothBrand {
+    const brand = new PhotoBoothBrand();
+
+    brand.name = name;
+    brand.is_event = is_event;
+
+    return brand;
+  }
+
+  static byId({ id }: FindBrandOptionProps): PhotoBoothBrand {
+    const brand = new PhotoBoothBrand();
+
+    brand.id = id;
+
+    return brand;
+  }
+
+  static byName({ name }: FindBrandOptionProps): PhotoBoothBrand {
+    const brand = new PhotoBoothBrand();
+
+    brand.name = name;
+
+    return brand;
+  }
 }
 
 @Entity('hashtag')
@@ -75,7 +147,11 @@ export class PhotoBoothHashtag {
   @JoinColumn({ name: 'photo_booth_brand_id' })
   photo_booth_brand: PhotoBoothBrand;
 
-  @ManyToOne(() => Hashtag, (hashtag: Hashtag) => hashtag.photo_booth_hashtags)
+  @ManyToOne(
+    () => Hashtag,
+    (hashtag: Hashtag) => hashtag.photo_booth_hashtags,
+    { eager: true },
+  )
   @JoinColumn({ name: 'hashtag_id' })
   hashtag: Hashtag;
 }
