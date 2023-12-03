a877f7d2f73ee4ed3be27a86465c94e0b5262f61
Chooooooo <suntail2002@naver.com>
2023-12-03 22:26:23 +0900

    Feat: 포토부스 브랜드 Dto 작성
    - 조회 list, detail response Dto 작성
    - 쿼리 Dto 작성
    - 생성 request Dto 작성
    - 수정 request Dto 작성
    
    Related to #4

--- src/photo-booth/dto/get-photo-booth-detail.dto.ts
+++ src/photo-booth/dto/get-photo-booth-detail.dto.ts
@@ -2,7 +2,10 @@ import { Exclude, Expose, Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 import { HiddenPhotoBooth } from '../entity/photo-booth-hidden.entity';
 import { PhotoBooth } from '../entity/photo-booth.entity';
-import { PhotoBoothBrand } from '../entity/photo-booth-brand.entity';
+import {
+  PhotoBoothBrand,
+  PhotoBoothHashtag,
+} from '../entity/photo-booth-brand.entity';
 
 export class GetPhotoBoothDetailDto {
   @Exclude() private readonly _id: string;
@@ -94,5 +97,79 @@ export class GetPhotoBoothDetailDto {
   @Type(() => PhotoBoothBrand)
   get brandName(): string | null {
     return this._photo_booth_brand.name;
+  }
+}
+
+export class GetBoothBrandDetailDto {
+  @Exclude() private readonly _id: number;
+  @Exclude() private readonly _name: string;
+  @Exclude() private readonly _main_thumbnail_image_url?: string | null;
+  @Exclude() private readonly _description?: string | null;
+  @Exclude() private readonly _photo_booth_url?: string | null;
+  @Exclude() private readonly _is_event: boolean;
+  @Exclude()
+  @Type(() => PhotoBoothHashtag)
+  private readonly _photo_booth_hashtags: PhotoBoothHashtag[] | null;
+
+  constructor(data: PhotoBoothBrand) {
+    Object.keys(data).forEach((key) => (this[`_${key}`] = data[key]));
+  }
+
+  @ApiProperty({ description: '포토부스 업체의 id 값' })
+  @Expose()
+  get id(): number {
+    return this._id;
+  }
+
+  @ApiProperty({
+    description: '포토부스의 업체명',
+    example: '하루필름',
+  })
+  @Expose()
+  get name(): string {
+    return this._name;
+  }
+
+  @ApiProperty({
+    description: '포토부스의 업체 설명',
+  })
+  @Expose()
+  get description(): string | null {
+    return this._description;
+  }
+
+  @ApiProperty({
+    description: '포토부스의 업체 관련 홈페이지 주소',
+  })
+  @Expose()
+  get photoBoothUrl(): string | null {
+    return this._photo_booth_url;
+  }
+
+  @ApiProperty({
+    description: '포토부스의 업체 이벤트 허용 여부',
+    example: true,
+  })
+  @Expose()
+  get isEvent(): boolean {
+    return this._is_event;
+  }
+
+  @ApiProperty({
+    description: '포토부스의 대표 이미지',
+  })
+  @Expose()
+  get mainThumbnailImageUrl(): string | null {
+    return this._main_thumbnail_image_url;
+  }
+
+  @ApiProperty({
+    description: '포토부스의 해시태그',
+  })
+  @Expose()
+  get hashtags(): string[] {
+    return (
+      this._photo_booth_hashtags?.map((hashtags) => hashtags.hashtag.name) || []
+    );
   }
 }
--- src/photo-booth/dto/patch-photo-booth.dto.ts
+++ src/photo-booth/dto/patch-photo-booth.dto.ts
@@ -1,6 +1,7 @@
 import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { BrandUpdateProps } from '../entity/photo-booth-brand.entity';
 
 export interface PhotoBoothUpdateProps {
   name: string;
@@ -50,6 +51,58 @@ export class UpdatePhotoBoothDto {
       location: this.location,
       street_address: this.streetAddress,
       road_address: this.roadAddress,
+    };
+  }
+}
+
+export class UpdateBoothBrandDto {
+  @ApiProperty({
+    description: '포토부스의 업체명',
+    example: '하루필름',
+  })
+  @Expose()
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @ApiProperty({
+    description: '포토부스의 업체 설명',
+  })
+  @Expose()
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    description: '포토부스의 업체 관련 홈페이지 주소',
+  })
+  @Expose()
+  @IsString()
+  @IsOptional()
+  photoBoothUrl?: string;
+
+  @ApiProperty({
+    description: '포토부스의 업체 이벤트 허용 여부',
+    example: true,
+  })
+  @Expose()
+  @IsBoolean()
+  @IsOptional()
+  isEvent?: boolean;
+
+  @ApiProperty({
+    description: '포토부스의 대표 이미지',
+  })
+  @Expose()
+  mainThumbnailImageUrl?: string;
+
+  getUpdateProps(): BrandUpdateProps {
+    return {
+      name: this.name,
+      description: this.description,
+      photo_booth_url: this.photoBoothUrl,
+      main_thumbnail_image_url: this.mainThumbnailImageUrl,
+      is_event: this.isEvent,
     };
   }
 }
