26303a666a80d3b82b68dc1ede6afadeb0054757
Chooooooo <suntail2002@naver.com>
2023-12-07 23:47:23 +0900

    Refactor: pagination response 변경
    - pagination entity 삭제
    - pagination response 정적 메서드로 entity 이동
    
    Related to #17

--- src/common/dto/pagination-res.dto.ts
+++ src/common/dto/pagination-res.dto.ts
@@ -1,34 +1,49 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
+import { PaginationProps } from './pagination-req.dto';
 
 export class Page<T> {
-  @Exclude() private readonly take: number;
-  @Exclude() private readonly count: number;
+  @Exclude() private readonly _take: number;
+  @Exclude() private readonly _count: number;
+  @Exclude() private readonly _results: T[];
+  @Exclude() private readonly _page: number;
 
-  constructor(page: number, take: number, count: number, results: T[]) {
-    this.page = page;
-    this.take = take;
-    this.count = count;
-    this.results = results;
+  constructor(pageProps: PaginationProps, count: number, results: T[]) {
+    this._page = pageProps.page;
+    this._take = pageProps.take;
+    this._count = count;
+    this._results = results;
   }
 
   @ApiProperty({
     description: '페이지당 항목 수가 적용된 결과 데이터',
   })
   @Expose()
-  public results: T[];
+  get results(): T[] {
+    return this._results;
+  }
 
   @ApiProperty({
     description: '현재 페이지',
   })
   @Expose()
-  public page: number;
+  get page(): number {
+    return this._page;
+  }
 
   @ApiProperty({
     description: '총 페이지 수',
   })
   @Expose()
   get totalPage(): number {
-    return Math.ceil(this.count / this.take);
+    return Math.ceil(this._count / this._take);
+  }
+
+  static create<T>(
+    pageProps: PaginationProps,
+    count: number,
+    results: T[],
+  ): Page<T> {
+    return new Page<T>(pageProps, count, results);
   }
 }
