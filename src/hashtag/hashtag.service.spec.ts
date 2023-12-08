d796cfd5acc4c97c518addf06da087ee37123144
Chooooooo <suntail2002@naver.com>
2023-12-08 20:32:13 +0900

    Test: 테스트 파일 업데이트
    - 레포지토리 모듈 추가
    
    Related to #17

--- src/hashtag/hashtag.service.spec.ts
+++ src/hashtag/hashtag.service.spec.ts
@@ -1,27 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HashtagService } from './hashtag.service';
 import { HashtagRepository } from './repository/hastag.repository';
+import { EntityToHashtagRepository } from './repository/entity-hashtag.repository';
 
 class MockHashtagRepository {}
+
+class MockEntityToHashtagRepository {}
 
 describe('HashtagService', () => {
   let hashtagService: HashtagService;
   let hashtagRepository: HashtagRepository;
+  let entityToHashtagRepository: EntityToHashtagRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         HashtagService,
         { provide: HashtagRepository, useClass: MockHashtagRepository },
+        {
+          provide: EntityToHashtagRepository,
+          useClass: MockEntityToHashtagRepository,
+        },
       ],
     }).compile();
 
     hashtagService = module.get<HashtagService>(HashtagService);
     hashtagRepository = module.get<HashtagRepository>(HashtagRepository);
+    entityToHashtagRepository = module.get<EntityToHashtagRepository>(
+      EntityToHashtagRepository,
+    );
   });
 
   it('should be defined', () => {
     expect(hashtagService).toBeDefined();
     expect(hashtagRepository).toBeDefined();
+    expect(entityToHashtagRepository).toBeDefined();
   });
 });
