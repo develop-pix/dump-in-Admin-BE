d49f9b30d29b47514244ed592018e100409e3f9c
Chooooooo <suntail2002@naver.com>
2023-12-08 23:48:11 +0900

    Chore: 모듈 재설정
    - 모듈 재설정

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -4,10 +4,17 @@ import { EventController } from './event.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventImage } from './entity/event-image.entity';
 import { Events } from './entity/event.entity';
+import { HashtagModule } from '../hashtag/hashtag.module';
+import { PhotoBoothModule } from 'src/photo-booth/photo-booth.module';
+import { EventRepository } from './repository/event.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EventImage, Events])],
+  imports: [
+    TypeOrmModule.forFeature([EventImage, Events]),
+    HashtagModule,
+    PhotoBoothModule,
+  ],
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, EventRepository],
 })
 export class EventModule {}
--- src/hashtag/hashtag.module.ts
+++ src/hashtag/hashtag.module.ts
@@ -4,10 +4,12 @@ import { HashtagController } from './hashtag.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hashtag } from './entity/hashtag.entity';
 import { HashtagRepository } from './repository/hastag.repository';
+import { EntityToHashtagRepository } from './repository/entity-hashtag.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Hashtag])],
   controllers: [HashtagController],
-  providers: [HashtagService, HashtagRepository],
+  providers: [HashtagService, HashtagRepository, EntityToHashtagRepository],
+  exports: [HashtagService],
 })
 export class HashtagModule {}
