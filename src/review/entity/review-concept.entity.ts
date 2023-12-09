0c4ebccc1645ff1f581746df839ad7fa2e0e8399
Chooooooo <suntail2002@naver.com>
2023-12-09 19:57:01 +0900

    Feat: 리뷰 엔티티 추가
    - 리뷰 이미지
    - 리뷰 컨셉
    - 리뷰 및 정적 메서드
    
    Related to #14

---
+++ src/review/entity/review-concept.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  Column,
+  OneToMany,
+} from 'typeorm';
+import { Review } from './review.entity';
+
+@Entity()
+export class Concept {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @OneToMany(
+    () => ReviewConcept,
+    (reviewConcept: ReviewConcept) => reviewConcept.concept,
+  )
+  review_concepts: ReviewConcept[];
+}
+
+@Entity()
+export class ReviewConcept {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Review, (review) => review.review_concepts)
+  @JoinColumn({ name: 'review_id' })
+  review: Review;
+
+  @ManyToOne(() => Concept, (concept) => concept.review_concepts, {
+    eager: true,
+  })
+  @JoinColumn({ name: 'concept_id' })
+  concept: Concept;
+}
---
+++ src/review/entity/review-image.entity.ts
@@ -0,0 +1,24 @@
+// review-image.entity.ts
+
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Review } from './review.entity';
+import { BaseDateEntity } from 'src/common/entity/common-date.entity';
+
+@Entity()
+export class ReviewImage extends BaseDateEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Review, (review) => review.review_images)
+  @JoinColumn({ name: 'review_id' })
+  review: Review;
+
+  @Column()
+  review_image_url: string;
+}
