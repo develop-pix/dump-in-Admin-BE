6f2074087cb00203428a859908cd544633605ed0
Chooooooo <suntail2002@naver.com>
2023-12-20 06:37:41 +0900

    Docs: Swagger 응답 데코레이터 수정
    - 429 응답 객체 추가
    - fail 프로퍼티 추가
    - 문서 버전 업데이트

--- src/common/config/swagger.config.ts
+++ src/common/config/swagger.config.ts
@@ -3,6 +3,6 @@ import { DocumentBuilder } from '@nestjs/swagger';
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Dump-In-Admin API')
   .setDescription('The Dump-In-Admin API document')
-  .setVersion('1.0')
+  .setVersion('1.2')
   .addServer('/api')
   .build();
