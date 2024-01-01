a4e000f781c620fdd47dc36ebac854f23ed79c54
Chooooooo <suntail2002@naver.com>
2024-01-02 03:03:31 +0900

    Rename: 세션 생성 Dto 경로 이동
    - 세션 Dto를 유저에서 분리
    - 세션 Dto를 Auth 모듈로 이동
    
    Related to #26

--- src/auth/dto/get-admin-session.dto.ts
+++ src/auth/dto/get-admin-session.dto.ts
@@ -1,6 +1,6 @@
 import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-import { User } from '../entity/user.entity';
+import { User } from '../../user/entity/user.entity';
 
 export class GetAdminSessionDto {
   @ApiProperty({
