9eb7e0814032cf910816e192c5cb9c4f8b528a02
Chooooooo <suntail2002@naver.com>
2024-01-04 02:04:03 +0900

    Docs: 파이프 주석 추가
    - auth.pipe.ts에 주석 추가
    - photo-booth.pipe.ts에 주석 추가
    
    Related to #25

--- src/auth/pipe/auth.pipe.ts
+++ src/auth/pipe/auth.pipe.ts
@@ -7,6 +7,12 @@ import { AuthService } from '../auth.service';
 import { LoginAdmin } from '../dto/post-login.dto';
 import { GetAdminSession } from '../dto/get-admin-session.dto';
 
+/**
+ * Validator supports service container in the case if want to inject dependencies into your custom validator constraint classes
+ * @see also https://github.com/leosuncin/nest-api-example/blob/master/src/blog/pipes/article.pipe.ts
+ * @see also https://github.com/typestack/class-validator?tab=readme-ov-file#using-service-container
+ * @see also https://docs.nestjs.com/techniques/validation
+ */
 @Injectable()
 export class AuthPipe
   implements PipeTransform<LoginAdmin, Promise<GetAdminSession>>
--- src/photo-booth/pipe/photo-booth.pipe.ts
+++ src/photo-booth/pipe/photo-booth.pipe.ts
@@ -2,6 +2,12 @@ import { ConflictException, Injectable, PipeTransform } from '@nestjs/common';
 import { PhotoBoothRepository } from '../repository/photo-booth.repository';
 import { PhotoBooth } from '../entity/photo-booth.entity';
 
+/**
+ * Validator supports service container in the case if want to inject dependencies into your custom validator constraint classes
+ * @see also https://github.com/leosuncin/nest-api-example/blob/master/src/blog/pipes/article.pipe.ts
+ * @see also https://github.com/typestack/class-validator?tab=readme-ov-file#using-service-container
+ * @see also https://docs.nestjs.com/techniques/validation
+ */
 @Injectable()
 export class PhotoBoothPipe
   implements PipeTransform<string, Promise<PhotoBooth['id']>>
